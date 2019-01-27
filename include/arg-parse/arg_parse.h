a9a57ac654dfb090c3162c70a5805fe44bd2ea80
Harsh Sinha <harshinha2@gmail.com>
2019-01-27 20:05:49 +0530

    working on argument parser

---
+++ include/arg-parse/arg_parse.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector.h>
+
+
+class ArgParser {
+ public:
+  ArgParser (int &argc, char **argv, std::map<std::string, std::string> args = None) {}
+}
